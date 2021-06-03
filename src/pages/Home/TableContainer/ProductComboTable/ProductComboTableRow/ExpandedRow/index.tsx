5e64d98e2f3238cd56ed38748d1d90ad63046f95
Thiago <thiagoodiniz@hotmail.com>
2021-06-03 16:54:32 -0300

    Montando conteudo de ExpandedRow

--- src/pages/Home/TableContainer/ProductComboTable/ProductComboTableRow/ExpandedRow/index.tsx
+++ src/pages/Home/TableContainer/ProductComboTable/ProductComboTableRow/ExpandedRow/index.tsx
@@ -1,6 +1,6 @@
 import { TableCell } from '@material-ui/core';
 import React from 'react';
-import { ExpandedTableRow } from './styles';
+import { ExpandedTableRow, TableCellContainer, ExpandedItemContainer, ExpandedItemTitle, ExpandedItemContent, SkuQtdContainer, VerticalDivisor, HalfVerticalDivisor } from './styles';
 
 interface IExpandedRowProps {
     isOddRow: boolean;
@@ -12,9 +12,86 @@ const ExpandedRow: React.FC<IExpandedRowProps> = ({ isOddRow }) => {
             className={isOddRow ? 'odd' : 'even'}
         >
             <TableCell />
-            <TableCell colSpan={8}>
-                Expanded content
+
+            <TableCell colSpan={7}>
+                <TableCellContainer>
+                    <ExpandedItemContainer>
+                        <ExpandedItemTitle>Imagem</ExpandedItemTitle>
+                        <ExpandedItemContent>
+                        </ExpandedItemContent>
+
+                        <VerticalDivisor />
+                    </ExpandedItemContainer>
+
+                    <ExpandedItemContainer>
+                        <ExpandedItemTitle>GPD</ExpandedItemTitle>
+                        <ExpandedItemContent>
+                            <span>- PREGO</span>
+                            <span>- VERGALHÃO</span>
+                        </ExpandedItemContent>
+
+                        <VerticalDivisor />
+                    </ExpandedItemContainer>
+
+                    <SkuQtdContainer>
+                        <ExpandedItemContainer>
+                            <ExpandedItemTitle>SKU / Quant</ExpandedItemTitle>
+                            <ExpandedItemContent>
+                                <span>- 3263215 / 180un.</span>
+                                <span>- 965463 / 250un.</span>
+                                <span>- 998989 / 20un.</span>
+                            </ExpandedItemContent>
+
+                            <HalfVerticalDivisor />
+                        </ExpandedItemContainer>
+
+                        <ExpandedItemContainer>
+                            <ExpandedItemTitle>&nbsp;</ExpandedItemTitle>
+                            <ExpandedItemContent>
+                                <span>- 669224 / 10un.</span>
+                                <span>- 998922 / 20un.</span>
+                                <span>- 36674 / 260un.</span>
+                            </ExpandedItemContent>
+
+                            <HalfVerticalDivisor />
+                        </ExpandedItemContainer>
+
+                        <ExpandedItemContainer>
+                            <ExpandedItemTitle>&nbsp;</ExpandedItemTitle>
+                            <ExpandedItemContent>
+                                <span>- 326315 / 180un.</span>
+                                <span>- 965463 / 250un.</span>
+                                <span>- 998989 / 20 un.</span>
+                            </ExpandedItemContent>
+                            
+                            <HalfVerticalDivisor />
+                        </ExpandedItemContainer>
+
+                        <ExpandedItemContainer>
+                            <ExpandedItemTitle>&nbsp;</ExpandedItemTitle>
+                            <ExpandedItemContent>
+                                <span>- 669224 / 10un.</span>
+                                <span>- 998922 / 20un.</span>
+                                <span>- 366974 / 260un.</span>
+                            </ExpandedItemContent>
+
+                            <VerticalDivisor />
+                        </ExpandedItemContainer>
+                    </SkuQtdContainer>
+
+                    <ExpandedItemContainer>
+                        <ExpandedItemTitle>E-commerce</ExpandedItemTitle>
+                        <ExpandedItemContent>
+                            <span>- MC1</span>
+                            <span>- Smartchain</span>
+                            <span>- Juntos somos mais</span>
+                        </ExpandedItemContent>
+                    </ExpandedItemContainer>
+                </TableCellContainer>
             </TableCell>
+
+            <TableCell />
+
         </ExpandedTableRow>
     );
 }
--- src/pages/Home/TableContainer/ProductComboTable/ProductComboTableRow/ExpandedRow/styles.ts
+++ src/pages/Home/TableContainer/ProductComboTable/ProductComboTableRow/ExpandedRow/styles.ts
@@ -11,3 +11,56 @@ export const ExpandedTableRow = styled(TableRow)`
         }
     }
 `;
+
+export const TableCellContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const ExpandedItemContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 6rem;
+    position: relative;
+`;
+
+export const ExpandedItemTitle = styled.h5`
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.colors.blueDark};
+    font-weight: 600;
+    margin-bottom: 0.5rem;
+`;
+
+export const ExpandedItemContent = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    & > span {
+        color: ${({ theme }) => theme.colors.blueGray};
+    }
+`;
+
+export const SkuQtdContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    & > div {
+        margin-right: 3rem;
+    }
+`;
+
+export const VerticalDivisor = styled.hr`
+    position: absolute;
+    right: -2.125rem;
+    height: 95%;  
+    top: 50%;
+    transform: translateY(-50%);
+    border-right: 1px solid ${({ theme }) => theme.colors.gray6};
+    opacity: 0.4;
+`;
+
+export const HalfVerticalDivisor = styled(VerticalDivisor)`
+    height: 40%;
+    top: 60%;
+    margin: 0 0.5rem;
+`;
